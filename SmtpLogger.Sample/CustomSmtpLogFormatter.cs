e7764b8c764f47cf4e2b27c7a8f00beb1a9a4878
Wojciech Berdowski <wberdowski@outlook.com>
2025-05-09 19:44:48 +0200

    Logger formats

---
+++ SmtpLogger.Sample/CustomSmtpLogFormatter.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace SmtpLogger.Sample;
+internal sealed class CustomSmtpLogFormatter : SmtpLogFormatter
+{
+    public override void Write<TState>(in LogEntry<TState> logEntry, IExternalScopeProvider? scopeProvider, TextWriter textWriter)
+    {
+        textWriter.WriteLine($"[{logEntry.LogLevel}] {logEntry.Category} {logEntry.Exception?.ToString()}");
+    }
+}
--- SmtpLogger/Formatters/DefaultSmtpLogFormatter.cs
+++ SmtpLogger/Formatters/DefaultSmtpLogFormatter.cs
@@ -31,16 +31,22 @@ namespace SmtpLogger.Formatters
 
             if (logEntry.Exception != null)
             {
-                textWriter.WriteLine("<tr>");
-                textWriter.WriteLine($"<td style=\"background-color: {accentColor}; vertical-align: middle;\"></td>");
-                textWriter.WriteLine($"<td style=\"padding: 0.5rem; font-family: sans-serif; font-size: 0.825rem;\">" +
-                    $"<pre>{logEntry.Exception.ToString()}</pre>" +
-                    $"</td>");
-                textWriter.WriteLine("</tr>");
+                WriteException(textWriter, accentColor, logEntry.Exception);
             }
 
             textWriter.WriteLine("</table>");
         }
+
+        private static void WriteException(TextWriter textWriter, string accentColor, Exception innerException)
+        {
+            textWriter.WriteLine("<tr>");
+            textWriter.WriteLine($"<td style=\"width: 0.125rem; background-color: {accentColor}; vertical-align: middle;\"></td>");
+            textWriter.WriteLine($"<td style=\"padding: 0.5rem; font-family: sans-serif; font-size: 0.825rem;\">" +
+                $"<pre>{innerException.ToString()}</pre>" +
+                $"</td>");
+            textWriter.WriteLine("</tr>");
+        }
+
         public void Dispose()
         {
 
